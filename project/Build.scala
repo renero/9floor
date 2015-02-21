faa837fb1b9c12fd296e0b786568f6c125bbe0dc
Daniel Micol Ponce <daniel.micolponce@telefonica.com>
2015-02-21 16:10:23 +0100

    Add project directory

---
+++ project/Build.scala
@@ -0,0 +1,33 @@
+import sbt._
+import sbt.Keys._
+import sbtassembly.Plugin._
+import sbtassembly.Plugin.AssemblyKeys._
+
+object Build extends sbt.Build {
+
+  def projectId(name: String) = s"konkordator-$name"
+
+  val KonkordatorMergeStrategy = mergeStrategy in assembly := {
+    case "reference.conf" => MergeStrategy.concat
+    case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
+    case m if m.toLowerCase.matches("meta-inf/services/.*") => MergeStrategy.concat
+    case m if m.toLowerCase.matches("meta-inf/.*\\.sf$") => MergeStrategy.discard
+    case _ => MergeStrategy.first
+  }
+
+  lazy val KonkordatorAssembly = assemblySettings ++ KonkordatorMergeStrategy
+
+  lazy val root = (
+    Project(
+      id = projectId("root"), base = file("."),
+      settings = Defaults.defaultSettings ++ sbtassembly.Plugin.assemblySettings ++ addArtifact(
+        Artifact(projectId("root"), "assembly"), sbtassembly.Plugin.AssemblyKeys.assembly)
+    )
+      settings(Defaults.itSettings: _*)
+      settings(KonkordatorAssembly: _*)
+    )
+
+  lazy val indices = (Project(id = projectId("indices"), base = file("indices"))
+    settings(Defaults.itSettings: _*)
+  )
+}
